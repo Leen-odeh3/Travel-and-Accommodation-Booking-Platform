cbb579c44583007af0b494952cdebca48ffa8451
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-07-23 23:01:44 +0300

    add Response to handle it

---
+++ HotelBookingPlatform.Domain/Bases/Response.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+namespace HotelBookingPlatform.Domain.Bases;
+public class Response
+{
+    public HttpStatusCode StatusCode { get; set; }
+    public object Data { get; set; }
+    public bool Succeeded { get; set; }
+    public string Message { get; set; }
+    public List<string> Errors { get; set; }
+}
