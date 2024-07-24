395989f19bf499675298e4bd1c42c462672e951d
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-07-24 19:25:44 +0300

    add validation when add new user

---
+++ HotelBookingPlatform.Application/Validator/RegisterUserValidator.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using HotelBookingPlatform.Domain.DTOs.Register;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBookingPlatform.Application.Validator;
+public class RegisterUserValidator : AbstractValidator<RegisterRequestDto>
+{
+    public RegisterUserValidator()
+    {
+        ApplyValidationRoles();
+    }
+
+    public void ApplyValidationRoles()
+    {
+        RuleFor(x => x.FirstName)
+            .NotEmpty().WithMessage("Username is required")
+            .Matches("^[a-zA-Z]").WithMessage("Name can only contain letters");
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Invalid email format");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required")
+            .MinimumLength(10).WithMessage("Password must be at least 10 characters long")
+            .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter")
+            .Matches("[a-z]").WithMessage("Password must contain at least one lowercase letter")
+            .Matches("[0-9]").WithMessage("Password must contain at least one digit")
+            .Matches("[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]").WithMessage("Password must contain at least one special character");
+    }
+}
