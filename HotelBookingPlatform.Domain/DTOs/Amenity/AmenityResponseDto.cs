2900b53279bbee3e97419b5487a1995e6fbfb241
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-07-28 13:33:51 +0300

    Update Code

--- HotelBookingPlatform.Domain/DTOs/Amenity/AmenityResponseDto.cs
+++ HotelBookingPlatform.Domain/DTOs/Amenity/AmenityResponseDto.cs
@@ -3,7 +3,6 @@
 namespace HotelBookingPlatform.Domain.DTOs.Amenity;
 public class AmenityResponseDto
 {
-    public int AmenityID { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public List<RoomClassAmenityDto> RoomClasses { get; set; }
