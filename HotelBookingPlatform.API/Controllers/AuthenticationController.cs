9ad7224e1d73464f7fbecb126cee8c6c2341a59c
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-07-29 23:36:22 +0300

    Auth Logic Complete

--- HotelBookingPlatform.API/Controllers/AuthenticationController.cs
+++ HotelBookingPlatform.API/Controllers/AuthenticationController.cs
@@ -7,6 +7,7 @@ using HotelBookingPlatform.Domain.IRepositories;
 using HotelBookingPlatform.Domain.Enums;
 using Swashbuckle.AspNetCore.Annotations;
 using HotelBookingPlatform.Domain.Exceptions;
+using Microsoft.AspNetCore.Authorization;
 namespace HotelBookingPlatform.API.Controllers;
 
 [Route("api/auth")]
@@ -34,6 +35,8 @@ public class AuthenticationController : ControllerBase
     }
 
     [HttpPost("login")]
+    [SwaggerOperation(Summary = "Authenticate User and Generate Token",
+    Description = "Authenticates the user with the provided email and password. If the credentials are valid, returns a token and user information. If the credentials are invalid, returns an unauthorized error.")]
     public async Task<IActionResult> Login([FromBody] LoginRequestDto loginRequestDto)
     {
         var loginResponse = await _userRepository.Login(loginRequestDto);
@@ -43,7 +46,9 @@ public class AuthenticationController : ControllerBase
     }
 
     [HttpPost("assign-admin")]
-    //[Authorize(Roles = "Admin")]
+    [Authorize(Roles = "Admin")]
+    [SwaggerOperation(Summary = "Assign Admin Role to User",
+    Description = "Assigns the admin role to a user identified by their email address. Only authorized administrators can perform this action. Returns a success message if the role is assigned successfully or throws an error if the user is not found or role assignment fails.")]
     public async Task<IActionResult> AssignAdmin([FromBody] AdminAssignmentRequestDto request)
     {
         var MainRole = Role.Admin.ToString();
