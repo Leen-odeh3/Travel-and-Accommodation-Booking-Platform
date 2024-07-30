c782cbeb443bb16d2641e1e2d616cf205e2be273
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-07-30 22:59:07 +0300

    remove column of city model

--- HotelBookingPlatform.Domain/Entities/City.cs
+++ HotelBookingPlatform.Domain/Entities/City.cs
@@ -8,6 +8,5 @@ public class City
     public string PostOffice { get; set; }
     public ICollection<Hotel> Hotels { get; set; } = new List<Hotel>();
     public DateTime CreatedAtUtc { get; set; }
-    public DateTime? ModifiedAtUtc { get; set; }
     public string CityImage {  get; set; }
 }
