a33bdbbf35a83cdf4aa257701929c08c80e08bf6
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-07-31 20:11:59 +0300

    Update CityMappingResponse

--- HotelBookingPlatform.API/Profiles/CityMappingProfile.cs
+++ HotelBookingPlatform.API/Profiles/CityMappingProfile.cs
@@ -1,13 +1,13 @@
 ﻿using AutoMapper;
 using HotelBookingPlatform.Domain.DTOs.City;
 using HotelBookingPlatform.Domain.Entities;
-
 namespace HotelBookingPlatform.API.Profiles;
 public class CityProfile : Profile
 {
     public CityProfile()
     {
-        CreateMap<City, CityResponseDto>();
+        CreateMap<City, CityWithHotelsResponseDto>()
+          .ForMember(dest => dest.Hotels, opt => opt.MapFrom(src => src.Hotels));
 
         CreateMap<CityCreateRequest, City>()
               .ForMember(dest => dest.CityImages,
