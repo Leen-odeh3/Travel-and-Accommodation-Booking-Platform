f1e78e1430cd0f0465c7bf4a15d97bd9dad83d30
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-08-20 23:22:34 +0300

    Add unit tests for Validator Requests.

---
+++ HotelBookingPlatformApplication.Test/EmailService/EmailServiceFixture.cs
@@ -0,0 +1,40 @@
+﻿using HotelBookingPlatform.Domain.DTOs.InvoiceRecord;
+namespace HotelBookingPlatform.Application.Tests;
+public class EmailServiceFixture
+{
+    public Mock<ILog> LogMock { get; private set; }
+    public EmailSettings EmailSettings { get; private set; }
+    public EmailService EmailService { get; private set; }
+
+    public EmailServiceFixture()
+    {
+        LogMock = new Mock<ILog>();
+
+        EmailSettings = new EmailSettings
+        {
+            FromAddress = "leenodeh287@gmail.com",
+            SmtpServer = "smtp.example.com",
+            Port = 587,
+            Username = "leenodeh287",
+            Password = "test123@"
+        };
+
+        var options = Options.Create(EmailSettings);
+        EmailService = new EmailService(options, LogMock.Object);
+    }
+
+    public BookingConfirmation CreateBookingConfirmation(string email)
+    {
+        return new BookingConfirmation
+        {
+            UserEmail = email,
+            ConfirmationNumber = "12345",
+            HotelName = "Dar El_Jeld",
+            HotelAddress = "123 Test St",
+            RoomType = "Suite",
+            CheckInDate = DateTime.UtcNow.AddDays(1),
+            CheckOutDate = DateTime.UtcNow.AddDays(2),
+            TotalPrice = 100m
+        };
+    }
+}
---
+++ HotelBookingPlatformApplication.Test/ValidatotTests/OwnerValidatorTests.cs
@@ -0,0 +1,60 @@
+﻿using FluentValidation.TestHelper;
+namespace HotelBookingPlatformApplication.Test.ValidatorTests;
+public class OwnerValidatorTests
+{
+    private readonly OwnerValidator _validator;
+    public OwnerValidatorTests()
+    {
+        _validator = new OwnerValidator();
+    }
+
+    [Theory]
+    [InlineData("", "First name is required")]
+    [InlineData("123", "First name can only contain letters")]
+    [InlineData("John", null)]
+    public void Should_Validate_FirstName(string firstName, string expectedErrorMessage)
+    {
+        // Arrange
+        var model = new OwnerCreateDto { FirstName = firstName };
+        var result = _validator.TestValidate(model);
+
+        if (expectedErrorMessage is not null)
+            result.ShouldHaveValidationErrorFor(x => x.FirstName)
+                .WithErrorMessage(expectedErrorMessage);
+        else
+
+            result.ShouldNotHaveValidationErrorFor(x => x.FirstName);
+    }
+
+    [Theory]
+    [InlineData("", "Last name is required")]
+    [InlineData("123", "Last name can only contain letters")]
+    [InlineData("Doe", null)]
+    public void Should_Validate_LastName(string lastName, string expectedErrorMessage)
+    {
+        var model = new OwnerCreateDto { LastName = lastName };
+        var result = _validator.TestValidate(model);
+
+        // Assert
+        if (expectedErrorMessage is not null)
+            result.ShouldHaveValidationErrorFor(x => x.LastName)
+                .WithErrorMessage(expectedErrorMessage);
+        else
+            result.ShouldNotHaveValidationErrorFor(x => x.LastName);
+    }
+
+    [Theory]
+    [InlineData("", "Email is required")]
+    [InlineData("leenodeh287@gmail.com", null)]
+    public void Should_Validate_Email(string email, string expectedErrorMessage)
+    {
+        var model = new OwnerCreateDto { Email = email };
+        var result = _validator.TestValidate(model);
+
+        if (expectedErrorMessage is not null)
+            result.ShouldHaveValidationErrorFor(x => x.Email)
+                .WithErrorMessage(expectedErrorMessage);
+        else
+            result.ShouldNotHaveValidationErrorFor(x => x.Email);
+    }
+}
