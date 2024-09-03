77b4b9ffaa82a252d82a0d975d86e904adca58f9
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-09-03 20:12:25 +0300

    Add foreign key constraints to Images table

--- HotelBookingPlatform.Application/Core/Abstracts/IImageService.cs
+++ HotelBookingPlatform.Application/Core/Abstracts/IImageService.cs
@@ -1,8 +1,8 @@
 ﻿namespace HotelBookingPlatform.Application.Core.Abstracts;
 public interface IImageService
 {
-    Task<ImageUploadResult> UploadImageAsync(IFormFile file, string imageType, int entityId);
-    Task<DeletionResult> DeleteImageAsync(string publicId);
-    Task<GetResourceResult> GetImageDetailsAsync(string publicId);
+    Task<ImageUploadResult> UploadImageAsync(IFormFile file, string entityType, int entityId);
+    Task<Image> GetImageByUniqueIdAsync(string uniqueId);
     Task<IEnumerable<Image>> GetImagesByTypeAsync(string type);
+    Task<DeletionResult> DeleteImageAsync(string uniqueId);
 }
--- HotelBookingPlatform.Infrastructure/Implementation/ImageRepository.cs
+++ HotelBookingPlatform.Infrastructure/Implementation/ImageRepository.cs
@@ -1,15 +1,28 @@
 ﻿namespace HotelBookingPlatform.Infrastructure.Implementation;
-public class ImageRepository:GenericRepository<Image> , IImageRepository
-{    public ImageRepository(AppDbContext context)
-        : base(context)
+public class ImageRepository : GenericRepository<Image>, IImageRepository
+{
+    public ImageRepository(AppDbContext context)
+        : base(context) { }
+
+    public async Task<Image> GetByUniqueIdAsync(string uniqueId)
     {
+        return await _appDbContext.Images
+            .FirstOrDefaultAsync(img => img.PublicId.Contains(uniqueId));
     }
-    public async Task<Image> GetByPublicIdAsync(string publicId)
-    {
-        return await _appDbContext.Set<Image>().FirstOrDefaultAsync(img => img.PublicId == publicId);
-    }
+
     public async Task<IEnumerable<Image>> GetImagesByTypeAsync(string type)
     {
-        return await _appDbContext.Images.Where(img => img.Type == type).ToListAsync();
+        return await _appDbContext.Images
+            .Where(img => img.Type == type)
+            .ToListAsync();
+    }
+    public async Task DeleteByUniqueIdAsync(string uniqueId)
+    {
+        var image = await GetByUniqueIdAsync(uniqueId);
+        if (image is not null)
+        {
+            _appDbContext.Images.Remove(image);
+            await _appDbContext.SaveChangesAsync();
+        }
     }
 }
